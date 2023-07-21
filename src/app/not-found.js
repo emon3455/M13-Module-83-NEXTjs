26be57bce7c69d00bdb65cdaac22aeca65e12eb9
Emon <emon.mhk69@gmail.com>
2023-07-21 12:55:55 +0600

    error page done

---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+    return (
+        <div>
+            Something Went wrong!!!
+        </div>
+    );
+};
+
+export default NotFoundPage;
